72e582afa7735b9d138d4db29bf32720a77e99e3
Amiel Brencis Salipande <absalipande@gmail.com>
2023-04-12 16:09:30 +0800

    feat: initial commit for implementing adding and denying endpoints

--- src/components/FriendRequests.tsx
+++ src/components/FriendRequests.tsx
@@ -1,8 +1,9 @@
 'use client';
 
 import { FC, useState } from 'react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import { Check, UserPlus, X } from 'lucide-react';
+import axios from 'axios';
 
 interface FriendRequestsProps {
   incomingFriendRequests: IncomingFriendRequest[];
@@ -13,10 +14,33 @@ const FriendRequests: FC<FriendRequestsProps> = ({
   incomingFriendRequests,
   sessionId,
 }) => {
-//   const router = useRouter();
-  const [friendRequests, setfriendRequests] = useState<IncomingFriendRequest[]>(
+  const router = useRouter();
+  const [friendRequests, setFriendRequests] = useState<IncomingFriendRequest[]>(
     incomingFriendRequests
   );
+
+  // Accepting friend requests
+  const acceptFriend = async (senderId: string) => {
+    await axios.post('/api/friends/accept', { id: senderId });
+
+    setFriendRequests((prev) =>
+      prev.filter((request) => request.senderId !== senderId)
+    );
+
+    router.refresh();
+  };
+
+  // Denying friend requests
+  const denyFriend = async (senderId: string) => {
+    await axios.post('/api/friends/deny', { id: senderId });
+
+    setFriendRequests((prev) =>
+      prev.filter((request) => request.senderId !== senderId)
+    );
+
+    router.refresh();
+  };
+
   return (
     <>
       {friendRequests.length === 0 ? (
@@ -27,17 +51,18 @@ const FriendRequests: FC<FriendRequestsProps> = ({
             <UserPlus className='text-black' />
             <p className='font-medium text-lg'>{request.senderEmail}</p>
             <button
+              onClick={() => acceptFriend(request.senderId)}
               aria-label='accept friend'
               className='w-8 h-8 bg-indigo-600 hover:bg-indigo-700 grid place-items-center rounded-full transition hover:shadow-md'
             >
-              {' '}
               <Check className='font-semibold text-white w-3/4 h-3/4' />
             </button>
+
             <button
+              onClick={() => denyFriend(request.senderId)}
               aria-label='deny friend'
               className='w-8 h-8 bg-red-600 hover:bg-red-700 grid place-items-center rounded-full transition hover:shadow-md'
             >
-              {' '}
               <X className='font-semibold text-white w-3/4 h-3/4' />
             </button>
           </div>
